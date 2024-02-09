bda36749a4d84a85ce0b7c7e0483ae89c0713090
”Enne” <EnneFofe10@gmail.com>
2024-02-08 22:10:25 -0300

    Menu abrível - Parte 2
    Menu feito e funcionando com sucesso para telas menores

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,9 +4,8 @@ const menu = document.getElementById('menu')
 
 // Aplicando a função de abrir o menu
 menu.addEventListener('click', () => {
-    if (nav.style.right = '-810%') {
-        nav.style.right = '0'
-        nav.style.opacity = '100%'
+    if (nav.style.display = 'none') {
+        nav.style.display = 'block'
     }
 })
 
@@ -24,19 +23,20 @@ menu.addEventListener('keypress', (tecla) => {
 
 // Sair do menu
 // Chamada das tags por ID's
-const titulo = window.document.querySelector('.titulo')
+const logo = window.document.querySelector('.logo')
 const nav = window.document.querySelector('.nav')
 const conteudo = window.document.getElementById('conteudo')
 const rodape = window.document.getElementById('rodape')
 
 // Adicionar o evento nelas
-titulo.addEventListener('click', sairDoMenu)
-nav.addEventListener('click', sairDoMenu)
-conteudo.addEventListener('click', sairDoMenu)
-rodape.addEventListener('click', sairDoMenu)
+if (innerWidth < 1200) {
+    logo.addEventListener('click', sairDoMenu)
+    nav.addEventListener('click', sairDoMenu)
+    conteudo.addEventListener('click', sairDoMenu)
+    rodape.addEventListener('click', sairDoMenu)
+}
 
 // Aplicando função de sair do menu
 function sairDoMenu() {
-    nav.style.left = '-180%'
-    nav.style.opacity = '0%'
+    nav.style.display = 'none'
 }
